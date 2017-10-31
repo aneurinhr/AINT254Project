e5f79a30268c27bcff899630987045117a1a937e
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2017-10-31 18:09:42 +0000

    Basic Menus
    Completed basic menus.  Only thing left is to make a proper level or more and to fix that bug

---
+++ WhiteboardWorldGame/Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+    public string nextScene;
+
+	public void Activate()
+    {
+        if (nextScene == "Quit")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
+}
