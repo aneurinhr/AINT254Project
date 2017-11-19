539c70c638fc24695e455cc555a860a0c66e9021
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2017-11-19 00:29:32 +0000

    Blue AI
    made its movement pattern.  Red is now broken.

---
+++ WhiteboardWorldGame/Assets/Scripts/BlueMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BlueMovement : MonoBehaviour {
+
+    public Transform[] travelLocations;
+
+    private int m_counter = 0;
+    private bool foward = true;
+
+    void Update () {
+        
+        GetComponent<NavMeshAgent>().destination = travelLocations[m_counter].position;
+
+        if ((transform.position.x == travelLocations[m_counter].position.x) && (transform.position.z == travelLocations[m_counter].position.z) && (foward == true))
+        {
+            if (m_counter == travelLocations.Length - 1)
+            {
+                foward = false;
+                m_counter--;
+            }
+            else
+            {
+                m_counter++;
+            }
+
+        }
+        else if ((transform.position.x == travelLocations[m_counter].position.x) && (transform.position.z == travelLocations[m_counter].position.z) && (foward == false))
+        {
+            if (m_counter == 0)
+            {
+                foward = true;
+                m_counter++;
+            }
+            else
+            {
+                m_counter--;
+            }
+
+        }
+    }
+}
