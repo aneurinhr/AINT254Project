7692fde34c80cafd74979619773d33898f4a86ba
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2017-12-03 14:25:51 +0000

    Menu's
    Created/Changed the menu system.  Looks a lot better.

---
+++ WhiteboardWorldGame/Assets/Scripts/CanvasSwapButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasSwapButton : MonoBehaviour {
+
+    public Canvas nextCanvas;
+    public Canvas currentCanvas;
+    public AudioClip button;
+
+    public void Activate()
+    {
+        if (nextCanvas == null)
+        {
+            GetComponent<AudioSource>().PlayOneShot(button);
+            StartCoroutine(ExecuteQuit());
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(button);
+            StartCoroutine(ExecuteReset());
+        }
+    }
+
+    IEnumerator ExecuteReset()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        nextCanvas.gameObject.SetActive(true);
+
+        currentCanvas.gameObject.SetActive(false);
+    }
+
+    IEnumerator ExecuteQuit()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        Application.Quit();
+    }
+}
