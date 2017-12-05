b57b0b75dee0b9d57b7adb7cf964401bffb73b26
aneurinhr <aneurinhr@users.noreply.github.com>
2017-12-05 16:25:50 +0000

    Score System
    Added a score system, plus its UI to the game.

---
+++ WhiteboardWorldGame/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public float timeLeft = 0;
+    public float friendsLeft = 0;
+    public bool CalcScore = true;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (CalcScore == false)
+        {
+            float score = timeLeft * friendsLeft;
+            gameObject.GetComponent<Text>().text = "Score: " + (int)score;
+            CalcScore = true;
+        }
+
+	}
+}
---
+++ WhiteboardWorldGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float timer;
+    public bool finished = false;
+    public float startTimer;
+    public GameObject Finish;
+
+    private void Start()
+    {
+        timer = startTimer;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (finished == false) {
+            timer = timer - Time.deltaTime;
+
+            if (timer < 0.0f)
+            {
+                Finish.GetComponent<Finish>().GameOver();
+            }
+
+            UpdateUITimer();
+        }
+
+    }
+
+    private void UpdateUITimer()
+    {
+        gameObject.GetComponent<Image>().fillAmount = (1/startTimer) * timer;
+    }
+
+}
