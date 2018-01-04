81fa84f5399669aa448f13b5dd219fd0e3436698
DESKTOP-OKF45T0\Alzhar <aneurin.hodgson-roch@students.plymouth.ac.uk>
2018-01-04 18:37:31 +0000

    Pen
    Pen changes colour based on what colour your using.  You select pens now by clicking them at the bottom.

--- WhiteboardWorldGame/Assets/Scripts/ChangeInk.cs
+++ WhiteboardWorldGame/Assets/Scripts/ChangeInk.cs
@@ -13,46 +13,95 @@ public class ChangeInk : MonoBehaviour {
     public GameObject redUI;
     public GameObject blueUI;
 
+    public GameObject Pen;
+    public GameObject particleLauncher;
+
     public Material blackInk;
     public Material redInk;
     public Material blueInk;
 
-    // Update is called once per frame
-    void Update() {
+    public Material penBlack;
+    public Material penRed;
+    public Material penBlue;
 
-        if (Input.GetButtonDown("1"))
+    private string m_colour = "Black";
+    private bool m_CanSelect = false;
+
+    private void FixedUpdate()
+    {
+        if ((Input.GetButton("Fire1")) && (m_CanSelect == true))
         {
-            GetComponent<ParticleLauncher>().decalPool = bluePool;
-            GetComponent<ParticleSystemRenderer>().material = blueInk;
+            ChangeInkType();
+        }
+    }
+
+    private void Update()
+    {
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
+
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            if (_hit.collider.tag == "BlackPen")
+            {
+                m_colour = "Black";
+                m_CanSelect = true;
+            }
+            else if (_hit.collider.tag == "BluePen")
+            {
+                m_colour = "Blue";
+                m_CanSelect = true;
+            }
+            else if (_hit.collider.tag == "RedPen")
+            {
+                m_colour = "Red";
+                m_CanSelect = true;
+            }
+            else
+            {
+                m_CanSelect = false;
+            }
+        }
+        else
+        {
+            m_CanSelect = false;
+        }
+    }
+
+    public void ChangeInkType()
+    {
+        if (m_colour == "Blue")
+        {
+            particleLauncher.GetComponent<ParticleLauncher>().decalPool = bluePool;
+            particleLauncher.GetComponent<ParticleSystemRenderer>().material = blueInk;
 
             blackUI.GetComponent<Image>().color = new Color(135, 135, 135);
             redUI.GetComponent<Image>().color = new Color(135, 135, 135);
             blueUI.GetComponent<Image>().color = Color.green;
 
-            Debug.Log("Blue");
+            Pen.GetComponent<Renderer>().material = penBlue;
         }
-        else if (Input.GetButtonDown("2"))
+        else if (m_colour == "Black")
         {
-            GetComponent<ParticleLauncher>().decalPool = blackPool;
-            GetComponent<ParticleSystemRenderer>().material = blackInk;
+            particleLauncher.GetComponent<ParticleLauncher>().decalPool = blackPool;
+            particleLauncher.GetComponent<ParticleSystemRenderer>().material = blackInk;
 
             blackUI.GetComponent<Image>().color = Color.green;
             redUI.GetComponent<Image>().color = new Color(135, 135, 135);
             blueUI.GetComponent<Image>().color = new Color(135, 135, 135);
 
-            Debug.Log("Black");
+            Pen.GetComponent<Renderer>().material = penBlack;
         }
-        else if (Input.GetButtonDown("3"))
+        else if (m_colour == "Red")
         {
-            GetComponent<ParticleLauncher>().decalPool = redPool;
-            GetComponent<ParticleSystemRenderer>().material = redInk;
+            particleLauncher.GetComponent<ParticleLauncher>().decalPool = redPool;
+            particleLauncher.GetComponent<ParticleSystemRenderer>().material = redInk;
 
             blackUI.GetComponent<Image>().color = new Color(135, 135, 135);
             redUI.GetComponent<Image>().color = Color.green;
             blueUI.GetComponent<Image>().color = new Color(135, 135, 135);
 
-            Debug.Log("Red");
+            Pen.GetComponent<Renderer>().material = penRed;
         }
-
-	}
+    }
 }
